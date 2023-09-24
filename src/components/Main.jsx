9e209709346a94249aa8e5b36852d1caaa1cb617
Jose Maria Novillo <josenovillo88@gmail.com>
2023-09-23 21:19:40 -0300

    new profile pic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -57,7 +57,7 @@ export const Main = () => {
 
         <div className='max-w-[700px] m-auto  w-full flex flex-col justify-center items-center lg:items-center'>
 
-          <img src="http://drive.google.com/uc?export=view&id=1b_1dQ_uCiQry7uSvWQjOeqNm96TUcFxz" className='rounded-full object-cover object-center h-48 w-48 md:h-64 md:w-64' alt="" />
+          <img src="http://drive.google.com/uc?export=view&id=165jQT9bpmnnrquNyi6zK_z-YHBGLoBfp" className='rounded-full object-cover object-center h-48 w-48 md:h-64 md:w-64' alt="" />
           <h1 className='sm:text-7xl text-4xl  my-2 font-bold text-white'>Jose Maria Novillo</h1>
           <h2 className='flex text-4xl sm:text-5xl font-normal text-yellow-300 mb-4 font-handjet'>
             <TypeAnimation
