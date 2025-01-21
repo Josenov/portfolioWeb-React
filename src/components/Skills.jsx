8ae065f8be3a275d555b1ba29d6e9e6fbcc65bc6
Josenov <josenovillo88@gmail.com>
2025-01-21 16:09:38 -0300

    icons color change

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,6 +8,15 @@ import adaptability from '../images/adaptability.png'
 import problemResolution from '../images/problem-resolution.png'
 import idiom from '../images/hello.png'
 import {useTranslation} from "react-i18next"
+import hmtl from '../images/html.png'
+import css from '../images/css.png'
+import js from '../images/js.png'
+import react from '../images/react.png'
+import nodejs from '../images/nodejs.png'
+import express from '../images/express.png'
+import postman from '../images/postman.png'
+import github from '../images/github.png'
+import mongo from '../images/mongo.png'
 
 
 
@@ -23,7 +32,7 @@ const Skills = () => {
             <p className='text-2xl text-center text-[white] mt-8 '>{t("skills.soft")}</p>
             <div className='flex justify-center gap-10 flex-wrap align-items mt-10 mb-[100px]'>
                 <div className='flex flex-col items-center'>
-                    <img className='h-16 w-16 md:h-[95px] md:w-auto mb-1'  src={teamWork} alt="" />
+                    <img className=' md:h-[95px] md:w-auto mb-1'   src={teamWork}  alt="Trabajo en equipo" />
                     <p className='text-white'>{t("skills.soft_skills.1")}</p>
                 </div>
                 <div className='flex flex-col items-center ' >
@@ -51,11 +60,12 @@ const Skills = () => {
 
             <div className='flex justify-center gap-5 flex-wrap align-items items-center'>
                 <div className='flex flex-col items-center' >
-                    <FaHtml5 className=' text-orange-500 h-16 w-16 md:h-24 md:w-auto' />
+                    <img src={hmtl} alt="" className='h-16 w-16 md:h-24 md:w-auto' />
+                    
                     <p className='text-white'>HTML</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <FaCss3 className=' text-blue-700 h-16 w-16 md:h-24 md:w-auto' />       
+                <img src={css} alt="" className='h-16 w-16 md:h-24 md:w-auto' />       
                     <p className='text-white'>CSS</p>
                 </div>
                 <div className='flex flex-col items-center' >
@@ -67,31 +77,31 @@ const Skills = () => {
                     <p className='text-white'>Tailwind CSS</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <FaJs className=' text-yellow-400 h-16 w-16 md:h-24 md:w-auto' />      
+                <img src={js} alt="" className='h-16 w-16 md:h-24 md:w-auto' />     
                     <p className='text-white'>JavaScript</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <FaReact className=' text-blue-400 h-16 w-16 md:h-24 md:w-auto' />     
+                <img src={react} alt="" className='h-16 w-16 md:h-24 md:w-auto' />     
                     <p className='text-white'>React</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <FaNodeJs className=' text-green-400 h-16 w-16 md:h-24 md:w-auto' />       
+                <img src={nodejs} alt="" className='h-16 w-16 md:h-24 md:w-auto' />       
                     <p className='text-white'>NodeJS</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png"  className='h-16 w-16 md:h-24 md:w-24' alt="" />      
+                    <img src={mongo}  className='h-16 w-16 md:h-24 md:w-24' alt="" />      
                     <p className='text-white'>MongoDB</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <img src="https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png"  className='h-16 w-16 md:h-24 md:w-24' alt="" />      
+                    <img src={express}  className='h-16 w-16 md:h-24 md:w-24' alt="" />      
                     <p className='text-white'>Express</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <img src="https://cdn.worldvectorlogo.com/logos/postman.svg"  className='h-16 w-16 md:h-24 md:w-24' alt="" />     
+                    <img src={postman}  className='h-16 w-16 md:h-24 md:w-24' alt="" />     
                     <p className='text-white'>Postman</p>
                 </div>
                 <div className='flex flex-col items-center' >
-                    <img src="https://icon-library.com/images/github-icon-white/github-icon-white-5.jpg"  className='h-16 w-16 md:h-24 md:w-24' alt="" />    
+                    <img src={github}  className='h-16 w-16 md:h-24 md:w-24' alt="" />    
                     <p className='text-white'>Github</p>
                 </div>
                 
