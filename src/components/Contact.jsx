cb72d32d55c258ee43adefb775a22586389ff98d
Josenov <josenovillo88@gmail.com>
2025-01-27 13:12:20 -0300

    variables fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,10 +21,10 @@ const Contact = () => {
         e.preventDefault();
 
         emailjs.sendForm(
-            process.env.REACT_APP_EMAILJS_SERVICE_ID,
-            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+            import.meta.env.VITE_EMAILJS_SERVICE_ID,
+            import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
             form.current,
-            process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            import.meta.env.VITE_EMAILJS_PUBLIC_KEY
         )
             .then((result) => {
                 console.log(result.text);
